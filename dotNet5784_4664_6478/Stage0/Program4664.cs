a6f5d153c5f9c24b75b721740823423d6c436081
efrat354 <efratk354@gmail.com>
2023-09-23 20:42:24 +0300

    Nmae:Program4664-stage0 Folder:"C:\Users\User\source\repos\miniProject\dotNet5784_4664_6478\dotNet5784_4664_6478\dotNet5784_4664_6478.sln" Status:added

---
+++ dotNet5784_4664_6478/Stage0/Program4664.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4664();
+            Wellcome6478();
+            Console.ReadKey();
+        }
+
+        static partial void Wellcome6478();
+        private static void Welcome4664()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
