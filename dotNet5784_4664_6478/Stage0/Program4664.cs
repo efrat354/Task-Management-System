0ca759dec4944dc76af043caf83a62b5e6823896
YaelRivka <yaelr5754@gmail.com>
2023-10-24 15:52:59 +0300

    finel changing after checking

--- dotNet5784_4664_6478/Stage0/Program4664.cs
+++ dotNet5784_4664_6478/Stage0/Program4664.cs
@@ -14,9 +14,8 @@ namespace Stage0
         private static void Welcome4664()
         {
             Console.WriteLine("Enter your name:");
-            string name = Console.ReadLine();
-            Console.ReadKey();  
-            Console.WriteLine("{0}, welcome to my first console application!!!", name);
+            string name = Console.ReadLine();  
+            Console.WriteLine("{0}, welcome to my first console application", name);
         }
     }
 }
