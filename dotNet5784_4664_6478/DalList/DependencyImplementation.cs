90c896ed01d02d88f89fc986b3751f3c7d0896e0
efrat354 <efratk354@gmail.com>
2023-10-31 16:04:19 +0200

    continue CRUD

--- dotNet5784_4664_6478/DalList/DependencyImplementation.cs
+++ dotNet5784_4664_6478/DalList/DependencyImplementation.cs
@@ -3,15 +3,15 @@ namespace Dal;
 using DalApi;
 using DO;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 public class DependencyImplementation : IDependency
 {
-    public int Create(Dependency item)
+    public int Create(Dependency item)//Add a new item to the dependency's list
     {
-        //for entities with auto id
-        int id = DataSource.Config.NextDependencyId;
-        Dependency copy = item with { Id = id };
-        DataSource.Dependencys.Add(copy);
+        int id = DataSource.Config.NextDependencyId;//Give the next continuous number as an id to the item.
+        Dependency copy = item with { Id = id };//create a new item
+        DataSource.Dependencys.Add(copy);//Add the new item to the list
         return id;
     }
 
@@ -20,18 +20,39 @@ public class DependencyImplementation : IDependency
         throw new NotImplementedException();
     }
 
+    //Recieves an ID and check if it exists in the list 
     public Dependency? Read(int id)
     {
-       return DataSource.Dependencys.Find(x => x.GetId() == id);
+        if (DataSource.Dependencys.Exists(x => x!.Id == id))//
+        {
+            return DataSource.Dependencys.Find(x => x!.Id == id);
+        }
+        return null;
     }
 
     public List<Dependency> ReadAll()
     {
-        throw new NotImplementedException();
+        List<Dependency> newList = new List<Dependency>(DataSource.Dependencys.Count);
+
+        DataSource.Dependencys.ForEach((item) =>
+        {
+            newList.Add(item!);
+        });
+        return newList;
     }
 
     public void Update(Dependency item)
     {
-        throw new NotImplementedException();
+        Dependency? reference = Read(item.Id);
+        if (reference!=null)
+        {
+            DataSource.Dependencys.Remove(reference);
+            DataSource.Dependencys.Add(item);
+        }
+        else
+        {
+            throw new Exception("The item to update does not exist in the system");
+        }
+        
     }
 }
