73ac7c2d5ccfb6ce8292b1ccaf8c95859db7f584
efrat354 <efratk354@gmail.com>
2023-11-27 15:26:17 +0200

    unit9

--- dotNet5784_4664_6478/DalFacade/DalApi/ICrud.cs
+++ dotNet5784_4664_6478/DalFacade/DalApi/ICrud.cs
@@ -4,8 +4,10 @@ namespace DalApi;
 public interface ICrud<T> where T : class
 {
     int Create(T item); //Creates a new dependency object
-    T? Read(int id); //Reads a dependency  by its ID 
-    List<T> ReadAll(); //Reads all the dependencies objects 
+    //T? Read(int id); //Reads a dependency  by its ID 
+    //List<T> ReadAll(); //Reads all the dependencies objects 
+    T? Read(Func<T, bool> filter); // stage 2
+    IEnumerable<T?> ReadAll(Func<T?, bool>? filter = null); // stage 2
     void Update(T item); //Updates dependency object
     void Delete(int id); //Deletes a dependency by its Id
 }
