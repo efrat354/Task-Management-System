9b53d4617380e57562298d5b64691aadf2caa837
YaelRivka <yaelr5754@gmail.com>
2023-11-28 19:07:45 +0200

    Stage2 Final commit

--- dotNet5784_4664_6478/DalFacade/DO/Exceptions.cs
+++ dotNet5784_4664_6478/DalFacade/DO/Exceptions.cs
@@ -1,12 +1,12 @@
 ﻿namespace DO;
 //A file for defining different classes of exception types
-//Add a new exception class in order to take care of cases of not exist input such as an entity with an ID number that does not exist in the list
+//Add a new exception class in order to take care of a case of an entity with an ID number that does not exist in the list
 [Serializable]
 public class DalDoesNotExistException : Exception
 {
     public DalDoesNotExistException(string? message) : base(message) { }
 }
-//Add a new exception class in order to take care of cases of input which already exists such as an entity with an ID number that already exists in the list
+//Add a new exception class in order to take care of a case an entity with an ID number that already exists in the list
 
 [Serializable]
 public class DalAlreadyExistsException : Exception
