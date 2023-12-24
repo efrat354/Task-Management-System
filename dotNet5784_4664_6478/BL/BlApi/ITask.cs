3235b427f8014d249fc8284521cbc7cbc4b38035
YaelRivka <yaelr5754@gmail.com>
2023-12-24 18:51:32 +0200

    unit 7ב7-א

---
+++ dotNet5784_4664_6478/BL/BlApi/ITask.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace BlApi;
+
+public interface ITask
+{
+    public IEnumerable<BO.Task> ReadAll(Func<DO.Task, bool>? filter = null);//האם צריך TaskInList?
+    public BO.Task Read(int id);
+    public void Create(BO.Task task);
+    public void Delete(int id);
+    public void Update(BO.Task task);
+
+}
