13f96deddbb02cf4288572166a43a76d4f3556f3
efrat354 <efratk354@gmail.com>
2023-12-24 19:42:35 +0200

    unit 8- till the implementation

---
+++ dotNet5784_4664_6478/BL/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,42 @@
+﻿using BlApi;
+
+namespace BlImplementation;
+
+internal class EngineerImplementation : IEngineer
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+    public void Create(BO.Engineer eng)
+    {
+        DO.Engineer doEngineer = new DO.Engineer
+               (boEngineer.Id, boStudent.Name, boStudent.Alias, boStudent.IsActive, boStudent.BirthDate);
+        try
+        {
+            int idStud = _dal.Student.Create(doStudent);
+            return idStud;
+        }
+        catch (DO.DalAlreadyExistsException ex)
+        {
+            throw new BO.BlAlreadyExistsException($"Student with ID={boStudent.Id} already exists", ex);
+        }
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Engineer Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.Engineer> ReadAll(Func<DO.Engineer, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(BO.Engineer eng)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ dotNet5784_4664_6478/BL/BlImplementation/MilestoneImplementation.cs
@@ -0,0 +1,20 @@
+﻿
+
+using BlApi;
+using BO;
+
+namespace BlImplementation;
+
+internal class MilestoneImplementation : IMilestone
+{
+    private DalApi.IDal _dal = DalApi.Factory.Get;
+    public Milestone Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Milestone Update(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
