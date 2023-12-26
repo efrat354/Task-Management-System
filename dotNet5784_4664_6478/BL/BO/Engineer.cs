a0c62498c9532c57d3259330084d5cb6aeae87f5
YaelRivka <yaelr5754@gmail.com>
2023-12-26 18:40:48 +0200

    engineer implemintation

--- dotNet5784_4664_6478/BL/BO/Engineer.cs
+++ dotNet5784_4664_6478/BL/BO/Engineer.cs
@@ -3,10 +3,10 @@
 public class Engineer
 {
     public int Id { get; init; }
-    public string  Name { get; set; }
-    public string? Email { get; set; }//כנ"ל
-    public EngineerExperience Level { get; set; }
-    public double Cost { get; set; }
+    public required string  Name { get; set; }
+    public required string Email { get; set; }//כנ"ל
+    public required EngineerExperience Level { get; set; }
+    public required double Cost { get; set; }
     public bool Active { get; set; }//לבדוק אם היינו צריכות את זה
     public TaskInEngineer? Task { get; set; }
     //public override string ToString() => this.ToStringProperty();
