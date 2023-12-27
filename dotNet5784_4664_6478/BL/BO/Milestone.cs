1bd1b0d9c5c8a8992681d04aba146f070123796f
efrat354 <efratk354@gmail.com>
2023-12-27 20:02:41 +0200

    continue implement engineer in BO

--- dotNet5784_4664_6478/BL/BO/Milestone.cs
+++ dotNet5784_4664_6478/BL/BO/Milestone.cs
@@ -8,12 +8,12 @@ public class Milestone
     public required string Alias { get; set; }
     public required string Description { get; set; }
     public required DateTime CreatedAtDate { get; init; }
-    public Status Status { get; set; }
-    public DateTime ?StartDate { get; set; }
-    public DateTime? ScheduledEndDate { get; set; }
+    public Status? Status { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? ForecastDate { get; set; }//ScheduledEndDate
     public DateTime? DeadlineDate { get; set; }
     public DateTime? CompleteDate { get; set; }
-    public double? CompletionPercentage{ get; set; }
+    public double? CompletionPercentage { get; set; }
     public string? Remarks { get; set; }
     public List<TaskInList>? Dependencies { get; set; }
 }
