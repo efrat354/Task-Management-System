590b4792f5321f91744b105db5d015b230ca2750
YaelRivka <yaelr5754@gmail.com>
2024-01-01 19:47:54 +0200

    milestone implementation

--- dotNet5784_4664_6478/BL/BlImplementation/MilestoneImplementation.cs
+++ dotNet5784_4664_6478/BL/BlImplementation/MilestoneImplementation.cs
@@ -9,13 +9,25 @@ namespace BlImplementation;
 internal class MilestoneImplementation : IMilestone
 {
     private DalApi.IDal _dal = DalApi.Factory.Get;
-
+    /*
+     var groupedDependencies = _dal.Dependency.ReadAll()
+     .OrderBy(dep => dep?.DependsOnTask)
+     .GroupBy(dep => dep?.DependentTask, dep => dep?.DependsOnTask, (id, dependency) => new { TaskId = id, Dependencies = dependency })
+     .ToList();
+     
+     */
     public void CreateSchedule()
     {
-        var dependenciesList = _dal.Dependency.ReadAll();
-                                from dep in dependenciesList
-                                group dep by dep.DependentTask into groups
-                                select new { groups.Key,list= groups.OrderBy()  };
+        BO.Milestone milestone;
+        //רשימת אוביקטים מקובצים לפי dependent
+        var dependenciesList = _dal.Dependency.ReadAll()
+            .OrderBy(dep => dep?.DependsOnTask)//מיון לפי המשימה שתלוים בה
+            .GroupBy(dep => dep?.DependentTask, dep => dep?.DependsOnTask, (id, dependency) => new { TaskId = id, Dependencies = dependency })
+            .ToList();
+
+        var distinctDependencies = dependenciesList.Distinct().ToList();//מחיקת כפילויות
+      //  var milestoneList = from dep in distinctDependencies
+                         //   select new BO.Milestone() { }//
     }
 
     public Milestone Read(int id)
