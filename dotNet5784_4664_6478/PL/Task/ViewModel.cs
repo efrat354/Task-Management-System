3a48969cc3994d53b8537c21d633f0312e8acbd1
efrat354 <efratk354@gmail.com>
2024-02-14 23:11:11 +0200

    Final commit stage: 4,5,6

--- dotNet5784_4664_6478/PL/Task/ViewModel.cs
+++ dotNet5784_4664_6478/PL/Task/ViewModel.cs
@@ -4,28 +4,36 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 
-
-
 namespace PL.Task
-
 {
+    /// <summary>
+    /// Represents a ViewModel class responsible for managing tasks and the current task for the task's window.
+    /// </summary>
     public class ViewModel : DependencyObject
     {
-
+        /// <summary>
+        /// Gets or sets the collection of tasks.
+        /// </summary>
         public ObservableCollection<BO.Task> Tasks { get; set; }
+        /// <summary>
+        /// Gets or sets the current selected task.
+        /// </summary>
         public BO.Task CurrentTask
         {
             get { return (BO.Task)GetValue(CurrentTaskProperty); }
             set { SetValue(CurrentTaskProperty, value); }
         }
-
+        /// <summary>
+        /// Dependency property for the CurrentTask property, enabling data binding.
+        /// </summary>
         public static readonly DependencyProperty CurrentTaskProperty =
                 DependencyProperty.Register("CurrentTask", typeof(BO.Task), typeof(ViewModel), new PropertyMetadata(null));
-
+        /// <summary>
+        /// Initializes a new instance of the ViewModel class.
+        /// </summary>
         public ViewModel()
         {
             Tasks = new ObservableCollection<BO.Task>();
-            
         }
     }
 }
